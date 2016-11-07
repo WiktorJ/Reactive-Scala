413913692fa3192739481aadc7bad3f85bd62d7c
wiktor <wiktor.jurasz@gmail.com>
2016-11-08 00:22:10 +0100

    Tests implementation started

--- auctions-system/build.sbt
+++ auctions-system/build.sbt
@@ -7,4 +7,5 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.11",
   "com.typesafe.akka" %% "akka-testkit" % "2.4.11" % "test",
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test")
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+  "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % "test")
--- auctions-system/src/test/scala/AuctionTest.scala
+++ auctions-system/src/test/scala/AuctionTest.scala
@@ -1,22 +1,32 @@
-import actors.auctions.AuctionActorFSM
-import akka.actor.ActorSystem
+import actors.Seller
+import actors.auctions.{AuctionActorFSM, Created, Uninitialized}
+import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.{ImplicitSender, TestActorRef, TestFSMRef, TestKit}
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 /**
   * Created by wiktor on 04/11/16.
   */
 class AuctionTest extends TestKit(ActorSystem("Auction"))
-  with WordSpecLike with BeforeAndAfterAll with ImplicitSender {
+  with WordSpecLike
+  with BeforeAndAfterAll
+  with ImplicitSender
+  with MockFactory {
 
   override def afterAll(): Unit = {
     system.terminate
   }
 
+  val price: BigDecimal = 42
+  val id: String = "some_id"
+  val actor = mock[ActorRef]
+
   "An Auction" must {
     "start in Created state with Uninitialized data" in {
-//      val fsm = TestFSMRef(new AuctionActorFSM(_, _, _))
-//      val mustBeTypedProperly: TestActorRef[TestFsmActor] = fsm
+      val fsm = TestFSMRef(new AuctionActorFSM(price, actor, id))
+      fsm.stateName == Created
+      fsm.stateData == Uninitialized
     }
   }
 
