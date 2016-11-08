625e9eb043dba88a43a4b09415817e4327aaec04
wjurasz <wjurasz@lbisa.com>
2016-11-08 19:42:57 +0100

    Added test for Auction

--- auctions-system/src/test/scala/AuctionTest.scala
+++ auctions-system/src/test/scala/AuctionTest.scala
@@ -1,9 +1,16 @@
+import java.util.concurrent.TimeUnit
+
 import actors.Seller
-import actors.auctions.{AuctionActorFSM, Created, Uninitialized}
+import actors.auctions._
+import akka.actor.FSM.StateTimeout
 import akka.actor.{ActorRef, ActorSystem}
-import akka.testkit.{ImplicitSender, TestActorRef, TestFSMRef, TestKit}
+import akka.testkit.{ImplicitSender, TestActorRef, TestFSMRef, TestKit, TestProbe}
+import common._
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.FiniteDuration
 
 /**
   * Created by wiktor on 04/11/16.
@@ -11,8 +18,7 @@ import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 class AuctionTest extends TestKit(ActorSystem("Auction"))
   with WordSpecLike
   with BeforeAndAfterAll
-  with ImplicitSender
-  with MockFactory {
+  with ImplicitSender {
 
   override def afterAll(): Unit = {
     system.terminate
@@ -20,13 +26,38 @@ class AuctionTest extends TestKit(ActorSystem("Auction"))
 
   val price: BigDecimal = 42
   val id: String = "some_id"
-  val actor = mock[ActorRef]
+  val sellerProbe = TestProbe()
 
   "An Auction" must {
     "start in Created state with Uninitialized data" in {
-      val fsm = TestFSMRef(new AuctionActorFSM(price, actor, id))
+      val fsm = TestFSMRef(new AuctionActorFSM(price, sellerProbe.ref, id))
       fsm.stateName == Created
       fsm.stateData == Uninitialized
+    }
+
+    "change state to Ignored when received Stop" in {
+      val fsm = TestFSMRef(new AuctionActorFSM(price, sellerProbe.ref, id))
+      fsm ! Stop
+      fsm.stateName == Ignored
+      fsm.stateData == Uninitialized
+    }
+
+    "send AuctionNotSold message to its parent" in {
+      val fsm = TestFSMRef(new AuctionActorFSM(price, sellerProbe.ref, id))
+      fsm ! Stop
+      fsm ! StateTimeout
+      sellerProbe.expectMsg(500 millis, AuctionNotSold(id))
+    }
+
+    "send AuctionSold message to its parent and goto Sold state" in {
+      val fsm = TestFSMRef(new AuctionActorFSM(price, sellerProbe.ref, id))
+      fsm ! Start
+      fsm ! Bid(price + 10)
+      fsm ! StateTimeout
+      sellerProbe.expectMsgPF(500 millis) {
+        case AuctionSold(_, newPrice, _) if newPrice == price + 10 => ()
+      }
+      fsm.stateName == Sold
     }
   }
 
